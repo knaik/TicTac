93b53616d508983c8dc07a05c165aea8db9c38ca
Karan Naik <kn172@rutgers.edu>
2018-04-08 21:17:47 -0400

    Calculates winner row-wise. still needs a lot of error checking

--- Board.java
+++ Board.java
@@ -17,9 +17,9 @@ public class Board{
 			while(totalMoves < 10){
 				System.out.println("Current player is "+ curPlayer);
 
-				System.out.println("row:");
+				System.out.print("row:");
 				int tempR = scan.nextInt();
-				System.out.println("col:");
+				System.out.print("\b col:");
 				int tempC = scan.nextInt();
 				//grid[tempR][tempC] = curPlayer;
 				setMove(curPlayer, tempR, tempC);
@@ -31,15 +31,48 @@ public class Board{
 				}
 				
 				System.out.println(this.toString());
+				checkWin();
 			}
 		}
 	
 	
-	
+	public void checkWin(){
+		for (int r = 0; r < 3; r++){
+			int tempSumX = 0;
+			int tempSumO = 0;
+
+			for (int c = 0; c < 3; c++){
+				if (getP(r,c) == 'X') {
+					tempSumX++;
+				}
+				if (getP(r,c) == 'O') {
+					tempSumO++;
+				}
+			}
+			
+			if (tempSumX  == 3) {
+				System.out.println("X won");
+				System.exit(0);
+
+			}
+			if (tempSumO  == 3) {
+				System.out.println("O won");
+				System.exit(0);
+
+			}
+			
+		}
+		}
+		
+		
 	
 	
 	public void setMove(char in, int r, int c){
 		grid[r][c] = in;
+	}
+	
+	public char getP(int row, int col){
+		return grid[row][col];
 	}
 	
 	
--- TicT.java
+++ TicT.java
@@ -5,9 +5,9 @@ public class TicT{
 		//System.out.println("Welcome to tictactaco");
 		Board gameOne = new Board();
 		//System.out.println(gameOne);
-		Scanner scan = new Scanner(System.in);
-		System.out.println("How many players?");
-		int players = scan.nextInt();
+		//Scanner scan = new Scanner(System.in);
+		//System.out.println("How many players?");
+		//int players = scan.nextInt();
 		gameOne.play();
 		
 	}
