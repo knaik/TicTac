7710247168c62ce74bc06b2ca0ed6d6d10d37ee4
Karan Naik <kn172@rutgers.edu>
2018-04-08 20:45:09 -0400

    print

--- Board.java
+++ Board.java
@@ -1,6 +1,27 @@
 public class Board{
+	private char[][] grid = new char[3][3];
+	
 		public Board(){
-			System.out.println("board created");
+			//System.out.println("board created");
+			this.clear();
+		}
+		
+		public void clear(){
+			for (int row = 0; row < 3; row++){
+				for (int col = 0; col < 3; col++){
+					grid[row][col] = '~';
+				}
+			}
+		}
+		
+		public String toString(){
+			String s = "";
+			s = s + grid[0][0] + "|" + grid[0][1] + "|" + grid[0][2] + "\n";
+			s += "-----\n";
+			s = s + grid[1][0] + "|" + grid[1][1] + "|" + grid[1][2] + "\n";
+			s += "-----\n";
+			s = s + grid[2][0] + "|" + grid[2][1] + "|" + grid[2][2] + "\n";
+			return s;
 		}
 
 }
--- TicT.java
+++ TicT.java
@@ -1,6 +1,9 @@
 public class TicT{
 	public static void main(String[] args){
-		System.out.println("Welcome to tictactaco");
+		//System.out.println("Welcome to tictactaco");
 		Board gameOne = new Board();
+		//System.out.println(gameOne);
+		System.out.println("How many players?");
+		
 	}
 }
